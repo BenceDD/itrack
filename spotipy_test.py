5991d5622a4168a246130163d0e38040a36b74c8
BenceDD <mikulasbence@gmail.com>
2017-10-14 23:31:54 +0200

    Now playing song received

--- spotipy_test.py
+++ spotipy_test.py
@@ -2,16 +2,21 @@ import json
 import spotipy
 import spotipy.util as util
 
-SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
+redirect_uri = 'http://localhost:8888/callback'
+spotify_scope = 'user-read-playback-state user-read-currently-playing'
 
 with open('../credentials.json', encoding='utf-8') as file:
     credentials = json.load(file)
-    SPOTIPY_CLIENT_ID = credentials['client_id']
-    SPOTIPY_CLIENT_SECRET = credentials['client_secret']
+    client_id = credentials['client_id']
+    client_secret = credentials['client_secret']
+    username = credentials['username']
 
-token = util.prompt_for_user_token(username, scope='', client_id = SPOTIPY_CLIENT_ID, client_secret = SPOTIPY_CLIENT_SECRET, redirect_uri = SPOTIPY_REDIRECT_URI)
+print(username)
 
+token = util.prompt_for_user_token(username, scope=spotify_scope, client_id = client_id, client_secret = client_secret, redirect_uri = redirect_uri)
 spotify = spotipy.Spotify(auth=token)
-result = spotify.search(q='artist:' + 'Akon', type='artist')
-print(result)
+
+result = spotify.current_playback()
+
+print(result['item']['artists'][0]['name'] + ' - ' + result['item']['name'])
 
