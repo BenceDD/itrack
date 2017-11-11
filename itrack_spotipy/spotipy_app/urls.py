806e3481433f7543cf221a3773052046fdcfd826
Mikulás Bence <mikulasbence@gmail.com>
2017-11-11 18:20:49 +0100

    Current song got by AJAX with fetch API

--- itrack_spotipy/spotipy_app/urls.py
+++ itrack_spotipy/spotipy_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^redirect_to_spotify',views.redirect_to_spotify,name = 'redirect_to_spotify'),
     url(r'^login',views.login,name = 'login'),
     url(r'^current_music',views.current_music,name = 'current_music'),
-    # AJAX hangling
+    # AJAX handling
     url(r'^ajax/get_playlist_by_name/$', views.get_playlist_by_name, name='get_playlist_by_name'),
+    url(r'^ajax/get_current_listening/$', views.get_current_listening, name='get_current_listening'),
 ] 
