f2e534aff5dba3fe2fe3377c5a5bab7d1e08f909
Seres Lóránt <sereslorant@gmail.com>
2017-11-22 23:59:37 +0100

    Megcsináltam, hogy rögtön spotify bejelentkezéssel fogadja a lap a felhasználót és utána rögtön a lejátszási listákra ugorjon. A token ellenőrzésére wrapper függvényt írtam.

--- itrack_spotipy/spotipy_app/urls.py
+++ itrack_spotipy/spotipy_app/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index,name = 'index'),
+    url(r'^$',views.current_music,name = 'current_music'),
     url(r'^redirect_to_spotify',views.redirect_to_spotify,name = 'redirect_to_spotify'),
     url(r'^login',views.login,name = 'login'),
-    url(r'^current_music',views.current_music,name = 'current_music'),
     # AJAX handling
     url(r'^ajax/get_user_playlists/', views.get_user_playlists, name='get_user_playlists'),
     url(r'^ajax/get_current_listening/', views.get_current_listening, name='get_current_listening'),
