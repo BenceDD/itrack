ae94066bc030edb3498ad0272343abd9466c170a
Mikulás Bence <mikulasbence@gmail.com>
2017-11-25 02:38:48 +0100

    Frontend cleaned

--- itrack_spotipy/static/view.js
+++ itrack_spotipy/static/view.js
@@ -1,142 +1,146 @@
 function View() {
-    var model = Model()
     var default_album_cover = 'https://lh3.googleusercontent.com/UrY7BAZ-XfXGpfkeWg0zCCeo-7ras4DCoRalC_WXXWTK9q5b0Iw7B0YQMsVxZaNB7DM=w300-rw'
 
-    function setSongCardTitle(title) {
-        $("#song_card_header").html(title)
+    // html setters/getters...
+    function set_song_card_header(message) { $('#song_card_header').html(message) }
+    function set_song_card_title(title) { $('#song_card_title').html(title) }
+    function set_song_card_content(content) { $('#song_card_content').html(content) }
+    function set_album_cover_src(src) { $("#album_cover").attr("src", src) }
+    function set_album_card_title(title) { $('#album_card_title').html(title) }
+    function set_album_card_content(content) { $('#album_card_content').html(content) }
+    function set_album_card_link_wp(link) { $('#album_card_link_wp').attr('href', link) }
+    function set_album_card_link_wd(link) { $('#album_card_link_wd').attr('href', link) }
+    function set_artist_card_title(title) { $('#artist_card_title').html(title) }
+    function set_artist_card_content(content) { $('#artist_card_content').html(content) }
+    function set_artist_card_link_wp(link) { $('#artist_card_link_wp').attr('href', link) }
+    function set_artist_card_link_wd(link) { $('#artist_card_link_wd').attr('href', link) }
+    function toggle_album_card(visible) { $('#album_card').toggle(visible) }
+    function toggle_artist_card(visible) { $('#artist_card').toggle(visible) }
+    function toggle_song_card_album_sparator(visible) { $('#song_card_album_sparator').toggle(visible) }
+    function toggle_album_artist_sparator(visible) { $('#album_artist_sparator').toggle(visible) }
+    function get_playlist_container() { return $('#playlist_container') }
+
+    function get_playlist_card_heading_id(playlist_id) { return 'playlist_heading_' + playlist_id }
+    function get_playlist_card_collapse_id(playlist_id) { return 'playlist_collapse_' + playlist_id }
+    function get_playlist_card_content_id(playlist_id) { return 'playlist_content_' + playlist_id }
+    function get_playlist_card_with_id(playlist_id) { return $('#' + get_playlist_card_content_id(playlist_id)) }
+
+    // code generators
+    function createPlaylistCard(parent_id, playlist_id, header_content, collapse_content) {
+        // This is create a single playlist item, fill with the given data. 
+        var collapse_id = get_playlist_card_collapse_id(playlist_id)
+        var heading_html = '<div class="card-header" role="tab" id="' + get_playlist_card_heading_id(playlist_id) +
+            '"><h6 class="mb-0"><a data-toggle="collapse" onclick="controller.fillPlaylistCardWithSongs(\'' + playlist_id + '\')" href="#' + collapse_id +
+            '" aria-expanded="true" aria-controls="' + collapse_id + '">' + header_content + '</a></h5></div>';
+        var collapse_html = '<div id="' + collapse_id + '" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#' + parent_id + 
+            '"><div id="' + get_playlist_card_content_id(playlist_id) + '" class="card-body">' + collapse_content + '</div></div>'
+        return '<p><div class="card bg-light">' + heading_html + collapse_html + '</div></p>';
     }
 
-    function updateSongCard(track, now_playlig) {
-        if (now_playlig)
-            setSongCardTitle('Jelenleg ezt hallgatod')
+    function makeTableRow(no, title, artists, album, track_id) {
+        artists_array = []
+        for (i in artists) 
+            artists_array.push(artists[i]['name'])
+
+        if (track_id)
+            return '<tr onclick="controller.updateSongCard(\'' + track_id + '\')"><th scope="row">' + no + '</th><td>' + title + '</td><td>' + 
+                artists_array.join(', ')  + '</td><td>' + album + '</td></tr>'
         else
-            setSongCardTitle('Lejátszási listáról')
-
-        artists = []
-        for (i in track['artists']) {
-            artists.push(track['artists'][i]['name'])
-        }
-
-        $("#song_card_top_header").html(artists.join(', ') + ' - ' +  track['title'])
-
-        var image_url = default_album_cover
-        if (track['image'] != null)
-            image_url = track['image']
-        document.getElementById('album_cover').src = image_url
+            return '<tr"><th scope="row" class="text-muted">' + no + '</th><td class="text-muted">' + title + '</td><td class="text-muted">' + 
+                artists_array.join(', ')  + '</td><td class="text-muted">' + album + '</td></tr>'
     }
 
-    function updateSongCardInfoWithContent(info) {
-        $("#song_card_top_content").html(info['song']['text'])
-        $("#song_card_first_footer_header").html(info['album']['label'])
-        $("#song_card_first_footer_content").html(info['album']['text'])
-        $("#song_card_second_footer_header").html(info['artist']['label'])
-        $("#song_card_second_footer_content").html(info['artist']['text'])
-    }
-
-    function updateSongCardInfoWithMessage(msg) {
-        $("#song_card_top_content").html(msg)
-        $("#song_card_first_footer_header").html(msg)
-        $("#song_card_first_footer_content").html(msg)
-        $("#song_card_second_footer_header").html(msg)
-        $("#song_card_second_footer_content").html(msg)
-    }
-
-    function playlistCardHeadingID(playlist_id) {
-        return 'track_heading_' + playlist_id
-    }
-
-    function playlistCardCollapseID(playlist_id) {
-        return 'track_collapse_' + playlist_id
-    }
-
-    function playlistCardContentID(playlist_id) {
-        return 'track_content_' + playlist_id 
-    }
-
-    function displayCards(user_playlists) {
-
-        function createPlaylistCard(parent_id, playlist_id, header_content, collapse_content) {
-            var collapse_id = playlistCardCollapseID(playlist_id)
-            // TODO: ez szörnyen néz ki.
-            var heading_html = '<div class="card-header" role="tab" id="' + playlistCardHeadingID(playlist_id) +
-                '"><h6 class="mb-0"><a data-toggle="collapse" onclick="view.fillPlaylistCardWithSongs(\'' + playlist_id + '\')" href="#' + collapse_id +
-                '" aria-expanded="true" aria-controls="' + collapse_id + '">' + header_content + '</a></h5></div>';
-            var collapse_html = '<div id="' + collapse_id + '" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#' + parent_id + 
-                '"><div id="' + playlistCardContentID(playlist_id) + '" class="card-body">' + collapse_content + '</div></div>'
-            return '<p><div class="card bg-light">' + heading_html + collapse_html + '</div></p>';
-        }
-        
-        // TODO: vagy JQuery vagy NE.
-        var table = $('#track_cards_div')
-        for (i in user_playlists) 
-            table.append(createPlaylistCard('track_cards_div', user_playlists[i]['playlist_id'], user_playlists[i]['name'], 'Betöltés...'));
-    }
-
-    function fillPlaylistWithTracklist(playlist_id, tracklist) {
-        function makeRow(no, title, artists, album, track_id) {
-            artists_array = []
-            for (i in artists) 
-                artists_array.push(artists[i]['name'])
-
-            if (track_id)
-                return '<tr onclick="view.updateSongCard(\'' + track_id + '\')"><th scope="row">' + no + '</th><td>' + title + '</td><td>' + 
-                    artists_array.join(', ')  + '</td><td>' + album + '</td></tr>'
-            else
-                return '<tr"><th scope="row" class="text-muted">' + no + '</th><td class="text-muted">' + title + '</td><td class="text-muted">' + 
-                    artists_array.join(', ')  + '</td><td class="text-muted">' + album + '</td></tr>'
-        }
-
+    function makeTableData(tracklist) {
         var artist_title = 'Előadó'
         var album_title = 'Album'
         var track_title = 'Cím'
 
-        var content = '<table class="table table-hover"><thead><tr><th scope="col">#</th><th scope="col">' + track_title + 
+        var table_data = '<table class="table table-hover"><thead><tr><th scope="col">#</th><th scope="col">' + track_title + 
                 '</th><th scope="col">' + artist_title + '</th><th scope="col">' + album_title + '</th></tr></thead><tbody>'
         for (i in tracklist) 
-            content += makeRow(String(Number(i) + 1), tracklist[i]['title'], tracklist[i]['artists'], tracklist[i]['album'], tracklist[i]['track_id'])
-        content += '</tbody></table>'
+            table_data += makeTableRow(String(Number(i) + 1), tracklist[i]['title'], tracklist[i]['artists'], tracklist[i]['album'], tracklist[i]['track_id'])
+        table_data += '</tbody></table>'
+        return table_data
+    }
 
-        document.getElementById(playlistCardContentID(playlist_id)).innerHTML = content
+    // state functions
+    function spotifyLoading() {
+        toggle_album_card(false)
+        toggle_artist_card(false)
+        toggle_song_card_album_sparator(false)
+        toggle_album_artist_sparator(false)
+        set_song_card_header('Betöltés...')
+        set_album_card_title('A jelenleg hallgatott szám címe')
+        set_song_card_content('Az adatok lekérése folyamatban. Itt fognak majd megjelenni az információk a kiválaszott számmal kapcsolatban.')
+        set_album_cover_src(default_album_cover)
+    }
+
+    function displaySong(track, now_playlig) {
+        if (now_playlig)
+            set_song_card_header('Jelenleg ezt hallgatod')
+        else
+            set_song_card_header('Lejátszási listáról')
+
+        artists = []
+        for (i in track['artists']) 
+            artists.push(track['artists'][i]['name'])
+
+        set_song_card_title(artists.join(', ') + ' - ' +  track['title'])
+
+        if (track['image'] != null)
+            set_album_cover_src(track['image'])
+        else 
+            set_album_cover_src(default_album_cover)
+    }
+
+    function displayPlaylistCards(user_playlists) {
+        var table = get_playlist_container()
+        for (i in user_playlists) 
+            table.append(createPlaylistCard('playlist_container', user_playlists[i]['playlist_id'], user_playlists[i]['name'], 'Betöltés...'));
+    }
+
+    function displayPlaylistCardContent(playlist_id, tracklist) {
+        var table_data = makeTableData(tracklist)
+        get_playlist_card_with_id(playlist_id).html(table_data)
+    }
+
+    function displaySongInfo(info) {
+        if ('text' in info['song'])
+            set_song_card_content(info['song']['text'])
+        else
+            set_song_card_content('Sajnos nem található adat ehhez a számhoz.')
+        
+        if ('label' in info['album'] && 'text' in info['album']) {
+            set_album_card_title('Album: ' + info['album']['label'])
+            set_album_card_content(info['album']['text'])
+            set_album_card_link_wp(info['album']['wiki'])
+            set_album_card_link_wd('https://www.wikidata.org/wiki/' + info['album']['id'])
+            toggle_song_card_album_sparator(true)
+            toggle_album_card(true)
+        }
+        
+        if ('label' in info['artist'] && 'text' in info['artist']) {
+            set_artist_card_title('Előadó: ' + info['artist']['label'])
+            set_artist_card_content(info['artist']['text'])
+            set_artist_card_link_wp(info['artist']['wiki'])
+            set_artist_card_link_wd('https://www.wikidata.org/wiki/' + info['artist']['id'])
+            toggle_album_artist_sparator(true)
+            toggle_artist_card(true)
+        }
     }
 
     return {
-        updateSongCard: function(track_id) {
-            setSongCardTitle('Betöltés...')
-            if (track_id == null) {
-                model.getCurrentPlaying().then(function(track) {
-                    updateSongCard(track, true)
-                    return track
-                }).then(function(track) {
-                    return model.getSongInfo(track)
-                }).then(function(info) {
-                    updateSongCardInfoWithContent(info)
-                }).catch(function(){
-                    alert('Nem tudom letölteni a wikidata adatokat!')
-                })
-            } else {
-                var track = model.getTrackByID(track_id)
-                updateSongCard(track, false)
-                model.getSongInfo(track).then(function(info) {
-                    updateSongCardInfo(info)
-                }).catch(function(){
-                    alert('Nem tudom letölteni a wikidata adatokat!')
-                })
-            }
+        spotifyLoading: spotifyLoading,
+        displaySong: displaySong,
+        displayPlaylistCards: displayPlaylistCards,
+        displayPlaylistCardContent: displayPlaylistCardContent,
+        displaySongInfo: displaySongInfo,
+        spotifyError: function() {
+            set_song_card_header('A Spotify sajnos nem éhető el')
+            set_song_card_title(':(')
         },
-        updatePlaylistCards: function() {
-            model.getUserPlaylist().then(function(playlists) {
-                displayCards(playlists)
-            }).catch(function(){
-                alert('Nem tudom frissíteni a playlisteket!')
-            })
-        },
-        fillPlaylistCardWithSongs: function(playlist_id) {
-            // this name 'fillPlaylistCardWithSongs' referred in the createPlaylistCard method!!
-            model.getPlaylistContentByID(playlist_id).then(function(tracklist) {
-                fillPlaylistWithTracklist(playlist_id, tracklist)
-            }).catch(function(){
-                alert('Nem tudom feltölteni adatokkal a playlisteket!')
-            })
+        wikidataError: function() {
+            set_song_card_header('Valami gond volt a WikiData adatok lekérése közben...')
         }
     }
 }
